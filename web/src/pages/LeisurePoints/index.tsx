d8e2798c75f1e610d1d9eae9a99cb45b504aaabd
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-08-19 12:34:52 -0300

    feat(web): create page header component

--- web/src/pages/LeisurePoints/index.tsx
+++ web/src/pages/LeisurePoints/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css'
+
 function LeisurePoints() {
     return (
-        <h1>Pontos de Lazer</h1>
+        <div id="page-leisure-points" className="container">
+            <PageHeader pageTitle="Pontos de Lazer" title="Lugares encontrados" totalPlaces={32} />
+        </div>
     )
 }
 
--- web/src/pages/UtilityPoints/index.tsx
+++ web/src/pages/UtilityPoints/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+
+import PageHeader from "../../components/PageHeader";
+
+import './styles.css'
 
 function UtilityPoints() {
     return (
-        <h1>Pontos de utilidades</h1>
-    )
+        <div id="page-utility-points" className="container">
+            <PageHeader pageTitle="Pontos de Utilidades" title="Lugares encontrados" totalPlaces={20} />
+        </div>
+    );
 }
 
-export default UtilityPoints
+export default UtilityPoints;
