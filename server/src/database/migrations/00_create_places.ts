fce205328fbe535a0c65bb1199a4c0820240a585
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-08-21 14:30:10 -0300

    feat(server): create tables in the database

---
+++ server/src/database/migrations/00_create_places.ts
@@ -0,0 +1,17 @@
+import Knex from 'knex'
+
+export async function up(knex: Knex) {
+    return knex.schema.createTable('places', table => {
+        table.increments('id').primary()
+        table.string('name').notNullable()
+        table.string('avatar').notNullable()
+        table.string('place').notNullable()
+        table.string('address').notNullable()
+        table.string('whatsapp').notNullable()
+        table.string('bio').notNullable()
+    })
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('places')
+}
