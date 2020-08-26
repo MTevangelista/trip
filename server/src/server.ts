c611ebb807860b7b4e616d628b8f46ea6862875f
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-08-26 13:33:51 -0300

    feat(server): add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 
 import placeRoute from './routes/place-route'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/places', placeRoute)
