7679061a7e606dd19dac37e3f414514be589026a
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-08-27 11:30:16 -0300

    fix(web): fix error in function that converts minutes to hours

--- web/src/utils/convertMinutesToHours.ts
+++ web/src/utils/convertMinutesToHours.ts
@@ -1,11 +1,8 @@
 import moment from 'moment'
 
 export default function convertMinutesToHours(time: number) {
-    let h = time / 60 | 0
-    let m = time % 60 | 0
-    const hourFormat = 'HH:mm'
-    const HourUtc = moment.utc().hours(h).minutes(m)
-    const localHour = HourUtc.local()
+    let h = time / 60 
+    let m = time % 60 
 
-    return localHour.format(hourFormat)
+    return moment.utc().hours(h).minutes(m).format("hh:mm A")
 }
